a92c650ef4a426dbb512f31a916f501cfc15f08e
Alexa Billings <alexa.billings@gmail.com>
2018-01-29 15:41:18 -0600

    firebase seed. ignore map stuff, it's not donegit add .

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,20 @@
 const router = require('express').Router()
 module.exports = router
+const firebaseDb = require('../firebase')
+const seedData = require('./mockActivitiesData')
 
 router.use('/users', require('./users'))
+
+router.post('/seed', (req, res, next) => {
+  firebaseDb.ref('trips').push(seedData.trip1)
+  firebaseDb.ref('trips').push(seedData.trip2)
+  firebaseDb.ref('trips').push(seedData.trip3)
+  .then(res.sendStatus(200))
+})
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
   next(error)
 })
+
