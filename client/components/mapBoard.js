73a0518a7063c1578b8ea90ab0ee1b6111998dea
Alexa Billings <alexa.billings@gmail.com>
2018-02-03 11:08:00 -0600

    map still broken, just updating

--- client/components/mapBoard.js
+++ client/components/mapBoard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getRoutes, subscribeToTripThunkCreator, unsubscribeToTripThunkCreator, fetchTrip, setMapActionCreator } from '../store'
+import reactMapboxGL, {Layer, Feature} from 'react-mapbox-gl'
 import mapboxgl from 'mapbox-gl'
 import firebase from '../firebase'
 
@@ -47,15 +48,15 @@ let getRoutesGeoJSON = (activities, routes) => {
 
 
 class MapBoard extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       activities: {},
-      style: {
-        positon: 'absolute',
-        width: '200vh',
-        height: '100vh'
-      },
+      // style: {
+      //   positon: 'absolute',
+      //   width: '200vh',
+      //   height: '100vh'
+      // },
       routesGeoJSON: {
         type: 'FeatureCollection',
         features: []
@@ -75,102 +76,82 @@ class MapBoard extends Component {
   }
 
   componentDidMount(){
-    //get map from mapbox
-    mapboxgl.accessToken = token;
-    //rendering specifications for mapbox
-    const mapConfig = {
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [-87.6354, 41.8885],
-      zoom: 5
-    };
+    //if there are no activities when this mounts, we need to get the activities
+    console.log('this.props.activities', this.props.activities)
+    let activities = this.props.activities.filter(activity => {
+      return activity.isActive
+    })
 
-    this.map = new mapboxgl.Map(mapConfig);
-    console.log('map', this.map)
-    this.props.setMapActionCreator(this.map)
+    let dates = {}
+    //create an arr of activity obj as keys on dates obj
+    activities.forEach(activity => {
+      if (!dates[activity.date]) {
+        dates[activity.date] = {};
+      }
+      if (!dates[activity.date][activity.time]) {
+        dates[activity.date][activity.time] = [activity];
+      } else {
+        dates[activity.date][activity.time].push(activity);
+      }
+    })
 
-    this.map.on('load', () => {
-      //dates object stores dates from the activities arr as keys
+    const days = Object.keys(dates);
 
-      let activities = this.props.activities.filter(activity => {
-        return activity.isActive
-      })
-
-      let dates = {}
-  //create an arr of activity obj as keys on dates obj
-      activities.forEach(activity => {
-        if (!dates[activity.date]) {
-          dates[activity.date] = {};
-        }
-        if (!dates[activity.date][activity.time]) {
-          dates[activity.date][activity.time] = [activity];
-        } else {
-          dates[activity.date][activity.time].push(activity);
-        }
-      })
-
-      const days = Object.keys(dates);
-
-      days.forEach(day => {
-        if (!dates.hasOwnProperty(day)) return;
-        dates[day].coordinates = times.filter(time => {
-          //this is where it sorts by time of day
-          return !!dates[day][time]})
-        .map(time =>
-          //this is concatenating the long lat and breaking them into arrays of 2 the join/split madness is coercing the value from str to num
-          dates[day][time].map(({ lat, long }) => ([+long, +lat])
-          ).join(';')
+    days.forEach(day => {
+      if (!dates.hasOwnProperty(day)) return;
+      dates[day].coordinates = times.filter(time => {
+        //this is where it sorts by time of day
+        return !!dates[day][time]})
+      .map(time =>
+        //this is concatenating the long lat and breaking them into arrays of 2 the join/split madness is coercing the value from str to num
+        dates[day][time].map(({ lat, long }) => ([+long, +lat])
         ).join(';')
-        .split(';')
+      ).join(';')
+      .split(';')
+    })
+
+    days.forEach(day => {
+      dates[day].coordinates = dates[day].coordinates.map(coordinate => {
+        coordinate = coordinate.split(',').map(item => +item)
+        return coordinate
       })
-
-
-      days.forEach(day => {
-        dates[day].coordinates = dates[day].coordinates.map(coordinate => {
-          coordinate = coordinate.split(',').map(item => +item)
-          return coordinate
-        })
-        let routes = dates[day].coordinates
-        // this.props.getRoutes(routes)
-        this.setState({routesGeoJSON: getRoutesGeoJSON(activities, routes)})
-      })
-
-      this.map.addSource('routes', {
-        type: 'geojson',
-        data: this.state.routesGeoJSON
-      });
-
-      this.map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: 'routes',
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#888',
-          'line-width': 8
-        }
-      })
+      let routes = dates[day].coordinates
+      // this.props.getRoutes(routes)
+      console.log('this.props.getRoutes', this.props.getRoutesGeoJSON)
+      this.setState({routesGeoJSON: getRoutesGeoJSON(activities, routes)})
     })
   }
 
   render() {
-    //somehow you need to get the source
-    return (
-      <div>
-        <h1>Map</h1>
-        <div style={this.state.style} ref={el => (this.mapContainer = el)} />
-      </div>
-    )
+    const Map = reactMapboxGL({accessToken: token})
+    console.log('activities', this.props.activities)
+    if (!this.props.activities.length){
+      return <div/>
+    } else {
+      return (
+        <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        center={[-87.6354, 41.8885]}
+        containerStyle={{
+          height: "75vh",
+          width: "75vw"
+        }}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+            <Feature coordinates={[-87.6354, 41.8885]}/>
+            {/* <Feature coordinates={[-87.5831, 41.7906]}/> */}
+          </Layer>
+        </Map>
+      )
+    }
   }
 }
 
-function mapStateToProps(state) {
+let mapStateToProps = state => {
   return {
-    activities: state.activities,
-    routes: state.routes
+    activities: state.activities
   };
 }
 
@@ -186,7 +167,6 @@ let mapDispatchToProps = dispatch => {
       dispatch(fetchTrip(tripId))
     },
     getRoutes(coordinates, numRoutes){
-      console.log(coordinates)
       dispatch(getRoutes(coordinates, numRoutes))
     },
     setMapActionCreator(map) {
