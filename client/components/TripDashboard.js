40a2ee45e817b91a33ffda19d408b589f12f90ce
Alyssa Venere Braun <alyssavenere@gmail.com>
2018-02-04 10:01:29 -0600

    styling

--- client/components/TripDashboard.js
+++ client/components/TripDashboard.js
@@ -19,17 +19,18 @@ class TripDashboard extends Component {
       <div>
       <h1>{this.props.trip.name}</h1>
       <div className="trip-dashboard-nav">
-        <NavLink to={`/trip/${trip.id}/mytrip`} className="nav-link">My Trip</NavLink>
+        <NavLink to={`/trip/${trip.id}/mytrip`} className="nav-link active">My Trip</NavLink>
         <NavLink to={`/trip/${trip.id}/ideas`} className="nav-link">Idea Board</NavLink>
         <NavLink to={`/trip/${trip.id}/calendar`} className="nav-link">Schedule</NavLink>
         <NavLink to={`/trip/${trip.id}/map`} className="nav-link">Map</NavLink>
         <NavLink to={`/trip/${trip.id}/itinerary`} className="nav-link">Itinerary</NavLink>
       </div>
       <Switch>
-        <Route path="/trip/:tripId/myTrip" component={Hotels} />
+        <Route path="/trip/:tripId/mytrip" component={Hotels} />
         <Route path="/trip/:tripId/calendar" component={CalendarBoard} />
         <Route path="/trip/:tripId/map" component={MapBoard} />
         <Route path="/trip/:tripId/ideas" component={IdeaBoard} />
+        <Route path="/trip/:tripId/itinerary" component={Itinerary} />
       </Switch>
       </div>
     )
